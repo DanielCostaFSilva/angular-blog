a5cf2ae64a95160f46d436d277540137b4c633c9
Danielcosta <danielcfsilva@gmail.com>
2023-11-24 21:19:40 -0300

    criacao do content

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,21 @@
+export const dataFake = [
+  {
+    id: '1',
+    photo: 'aaa',
+    title: 'Novo Homen de Ferro',
+    description:
+      'Marvel anuncia novo Homem de Ferro com Robertinho no papel principal',
+    photoCover:
+      'https://t.ctcdn.com.br/jSEs-a2AsizaO2xZCQXcdbGPZW0=/i490793.jpeg',
+  },
+
+  {
+    id: '2',
+    photo: 'https://t.ctcdn.com.br/jSEs-a2AsizaO2xZCQXcdbGPZW0=/i490793.jpeg',
+    title: 'Saiu o novo filme 2',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent condimentum, tortor eu posuere feugiat, metus dolor dignissim massa, ut placerat lorem dolor eu sapien. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin sed libero non massa maximus maximus. Donec a quam eu eros gravida vulputate at vitae sem. Duis egestas lacinia cursus. Sed a nisi non urna aliquam pharetra. Duis a turpis tortor. Etiam quis leo at mauris dictum lacinia id a sem. Proin aliquet mattis nisl et dignissim. Aliquam non aliquam lacus. Donec quis justo at quam luctus tempor. Fusce sit amet pretium orci, quis ornare enim. Suspendisse sapien lorem, rhoncus eu euismod a, sagittis luctus sem. Ut suscipit eu urna malesuada rutrum. Suspendisse a neque rutrum, ornare lacus nec, pulvinar ex.',
+    photoCover:
+      'https://disneyplusbrasil.com.br/wp-content/uploads/2021/03/Tony-Stark-Cancelado.jpg',
+  },
+];
